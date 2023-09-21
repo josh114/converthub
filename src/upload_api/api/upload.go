889ad78af0ee6884afa99e04f18d588349a5b8b4
josh114 <freejosh02@gmail.com>
2023-09-21 15:00:18 +0100

    added upload logic

---
+++ src/upload_api/api/upload.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Upload(c *fiber.Ctx) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(400).SendString(fmt.Sprintf("an error occurred while uploading the file: %s", err))
+	}
+	fmt.Printf("Filename: %s\n", file.Filename)
+	fmt.Printf("Size: %d\n", file.Size)
+	fmt.Printf("MIME: %s\n", file.Header["Content-Type"])
+
+	savePath := filepath.Join("./uploads", file.Filename)
+	if err := c.SaveFile(file, savePath); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to save file",
+		})
+	}
+
+	c.Status(201).JSON(fiber.Map{
+		"message": "File uploaded successfully",
+		"filename": file.Filename,
+	})
+	return nil
+}
--- src/upload_api/main.go
+++ src/upload_api/main.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/josh114/converthub/src/upload_api/database"
+	"github.com/josh114/converthub/src/upload_api/router"
 )
 
 
 func main () {
 	database.ConnectDb()
-	app:= fiber.New()
-
+	app:= fiber.New(fiber.Config{
+		BodyLimit: 1000 * 1024 *1024,
+	})
+	router.SetupRoutes(app)
 	log.Fatal(app.Listen(":9010"))
 }
